dd593c1dac3a96af25c9a1257658368890550c23
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-18 18:33:31 +0530

    adding variable conversation file

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1::Hi ra, ela unnav"
+echo "$PERSON2::Hi ra, bane unna ra"
+echo "$PERSON1::eemadhya, edina nerchukuntunava ra"
+echo "$PERSON2::avunu ra, learning devscops"
