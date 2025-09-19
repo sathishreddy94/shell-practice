266f8f39562b1384778776b8884d2f3582b97bee
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-19 22:05:26 +0530

    variables new file adding

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::welcome to alaska, $PERSON2"
+echo "$PERSON2::Thank you $PERSON1, finally not on webinar"
+echo "$PERSON1::so, do we have a deal?"
+echo "$PERSON2::Yes, if u make ukraine joins Russia"
