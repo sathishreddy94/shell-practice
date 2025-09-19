0a6bec1f4a108704c1a99cb9bc62b7d6647ecdbb
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-19 22:27:58 +0530

    mask pin file added

---
+++ 06-variables.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+echo "Please enter your pin number"
+
+read -s PIN_NUMBER #PIN_NUMBER IS A VARIABLE --> it will not show pin number while entering
+
+echo "PIN entered is : $PIN_NUMBER"
