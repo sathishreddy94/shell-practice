bcdaae89aeeb0cf851b5956e1a8bf755cf119d19
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-19 22:22:12 +0530

    variables pin number new file edited

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Please enter your pin number"
-read PIN_NUMBER #PIN_NUMBER IS A VARIABLE
+#read PIN_NUMBER #PIN_NUMBER IS A VARIABLE --> it will show pin number while entering
+
+read -s PIN_NUMBER #PIN_NUMBER IS A VARIABLE --> it will not show pin number while entering
 
 echo "PIN entered is : $PIN_NUMBER"
