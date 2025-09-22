03f5a1510dbd085f1b0182eff6478d46952b5d22
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-22 13:29:47 +0530

    time execution script added

--- 08-variables.sh
+++ 08-variables.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
+START_TIME=$(date+%s)
 
+sleep 10 
+
+END_TIME=$(date+%s)
+
+TOTAL_TIME=$(($START_TIME-$END_TIME))
+
+echo "script execution time : $TOTAL_TIME in seconds"
 
 echo "Timestamp executed :: $DATE"
 
