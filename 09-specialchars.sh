360bab0edd63eba3d3c60533005a01f2987d8f64
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-23 11:23:13 +0530

    special variable file

--- 09-specialchars.sh
+++ 09-specialchars.sh
@@ -6,3 +6,9 @@ echo "All variables passed to this script: $*"
 
 echo "script name : $0"
 
+echo "Present working directory :: $PWD"
+
+echo "who is running this :: $USER"
+
+
+echo "user home directory :: $HOME"
