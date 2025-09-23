a56184c32b9548a88865e61acea67281d6dbc451
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-23 13:39:35 +0530

    prime number script added

---
+++ 13-primenumber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "enter a number : $NUMBER"
+
+read NUMBER
+
+
+if [ $(($NUMBER % 1) && ($NUMBER % $NUMBER)) -eq 0 ]; then
+    echo "given $NUMBER is prime number"
+else
+      echo "given $NUMBER is not prime number"
+fi
+ 
+
