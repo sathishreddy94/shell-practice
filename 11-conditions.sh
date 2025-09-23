efb5ceb7a63e0bea9d5ddc17cf9779fd62b638cf
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-23 13:00:15 +0530

    if else condition class is adding

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -2,11 +2,8 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ]; then
-
-echo "give number is $NUMBER is less than 10"
-
-else 
-echo "give number is $NUMBER is more than/greater than 10"
-
+if [ "$NUMBER" -lt 10 ]; then
+    echo "✅ Given number $NUMBER is less than 10"
+else
+    echo "✅ Given number $NUMBER is greater than or equal to 10"
 fi
