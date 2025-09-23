7165cb6a7cc0c209e80a53103abc04116eee2bd6
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-23 13:15:47 +0530

    even number odd number script added

---
+++ 12-EvenOdd.sh
@@ -0,0 +1,11 @@
+#!bash/bin
+
+echo "enter a number: $NUMBER"
+
+read NUMBER
+
+if [ $NUMBER%2 -eq 0]; then
+    echo "given $NUMBER is even number"
+else
+      echo "given $NUMBER is odd number"
+fi
