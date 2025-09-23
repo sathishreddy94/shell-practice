cc2a19e85bbb942cbd569e081630d59a693804e6
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-23 13:06:12 +0530

    if else condition class is adding

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,9 +4,13 @@ NUMBER=5
 
 if [ $NUMBER -lt 10 ]; then
 
-    echo "give number is $NUMBER is less than 10"
+    echo "given number is $NUMBER is less than 10"
+
+elif [ $NUMBER -eq 10 ]; then
+
+    echo "given number is $NUMBER is equal to 10"
 
 else 
-    echo "give number is $NUMBER is more than/greater than 10"
+    echo "given number is $NUMBER is more than/greater than 10"
 
 fi
