743d858530dbf1b246ab3203b81be0c101365c31
sathishreddy94 <sathishraghuram15@gmail.com>
2025-09-26 11:37:15 +0530

    colors code script is added

--- 16-colors.sh
+++ 16-colors.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 R="\e[31m"
-R="\e[32m"
+G="\e[32m"
 Y="\e[33m"
-Y="\e[0m"
+N="\e[0m"
 
-echo -e "\e[31m Hello World" # if we give like this following lines also will be in same color
+echo -e "$G Hello World" # if we give like this following lines also will be in same color
 echo "check this color"
 
 
-echo -e "\e[31m Hello World $N" # if we give like this color is limited only this line alone
+echo -e "$G Hello World $N" # if we give like this color is limited only this line alone
 echo "check this color"
